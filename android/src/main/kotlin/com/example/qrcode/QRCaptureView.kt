b89b18f96571eff8eda5f8bff045597f73269f45
chubzchoc <susy_valentz@yahoo.co.id>
2024-07-27 22:40:45 +0700

    Update QRCaptureView.kt

--- android/src/main/kotlin/com/example/qrcode/QRCaptureView.kt
+++ android/src/main/kotlin/com/example/qrcode/QRCaptureView.kt
@@ -159,31 +159,31 @@ class QRCaptureView(
         checkAndRequestPermission(null)
         activity.application.registerActivityLifecycleCallbacks(
             object : Application.ActivityLifecycleCallbacks {
-                override fun onActivityPaused(p0: Activity?) {
+                override fun onActivityPaused(p0: Activity) {
                     if (p0 == activity) {
                         barcodeView?.pause()
                     }
                 }
 
-                override fun onActivityResumed(p0: Activity?) {
+                override fun onActivityResumed(p0: Activity) {
                     if (p0 == activity) {
                         barcodeView?.resume()
                     }
                 }
 
-                override fun onActivityStarted(p0: Activity?) {
+                override fun onActivityCreated(p0: Activity, p1: Bundle?) {
                 }
 
-                override fun onActivityDestroyed(p0: Activity?) {
+                override fun onActivityStarted(p0: Activity) {
                 }
 
-                override fun onActivitySaveInstanceState(p0: Activity?, p1: Bundle?) {
+                override fun onActivityDestroyed(p0: Activity) {
                 }
 
-                override fun onActivityStopped(p0: Activity?) {
+                override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {
                 }
 
-                override fun onActivityCreated(p0: Activity?, p1: Bundle?) {
+                override fun onActivityStopped(p0: Activity) {
                 }
 
             }
